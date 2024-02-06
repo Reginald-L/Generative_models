233817f83eaee38cbb9e0b6f6cdf72bfddc73804
Reggie <liruijun@infimind.com>
2024-02-06 14:06:20 +0800

    [update]update train script

--- sd/train.py
+++ sd/train.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import os
 sys.path.insert(0, '/home/liruijun/projects/Generative_models')
@@ -11,6 +12,8 @@ from unet import Model
 from sd.diffusion_schedule import LinearScheduler
 from collections import OrderedDict
 from tqdm import tqdm
+import numpy as np
+from torchvision.utils import make_grid, save_image
 
 import copy
 
@@ -27,11 +30,17 @@ trans = transform.Compose(
 img_dataset = ImgDataset(dir='/home/liruijun/projects/datasets/flowers/train', transform=trans)
 data_loader = DataLoader(img_dataset, batch_size=128, shuffle=True)
 
+
+def init_weights(m):
+    if type(m) == nn.Linear or type(m) == nn.Conv2d:
+        nn.init.xavier_normal_(m.weight)
+
 device = torch.device('cuda:0')
 lr = 1e-3
-epochs = 100
+epochs = 200
 
 model = Model()
+model.apply(init_weights)
 ema_model = copy.deepcopy(model)
 model = model.to(device)
 ema_model = ema_model.to(device)
@@ -40,7 +49,7 @@ optimizer = Adam(model.parameters(), lr=1e-3)
 
 loss = nn.MSELoss()
 
-T = 500
+T = 1000
 diffusion_times = [x / T for x in range(T)]
 scheduler = LinearScheduler(diffusion_times)
 noise_rates, signal_rates = scheduler.diffusion_scheduler()
@@ -75,13 +84,14 @@ def update_ema_parameters():
         ema_parms[name] = 0.999 * ema_parms[name] + (1 - 0.999) * param
     ema_model.load_state_dict(ema_parms, strict=False)
 
-image_table = wandb.Table()
+ts = [t for t in range(0, T)]
 for epoch in tqdm(range(epochs)):
     model.train()
+    ts = random.sample(ts, 10)
     for i, imgs in enumerate(data_loader):
         optimizer.zero_grad()
         bsize = imgs.shape[0]
-        for t in tqdm(range(T)):
+        for t in tqdm(ts):
             noise_imgs, epsilons = forward_process(imgs, t)
             pred_noise = model(noise_imgs, torch.tensor([t]*bsize, device=device).reshape((-1, 1)))
             loss_noise = loss(pred_noise, epsilons)
@@ -89,13 +99,12 @@ for epoch in tqdm(range(epochs)):
             optimizer.step()
             update_ema_parameters()
         wandb.log({"loss": loss_noise})
-    
-    image_table.add_column('noise_imgs', noise_imgs)
-    image_table.add_column('epsilons', epsilons)
-    image_table.add_column('pred_noise', pred_noise)
-    wandb.log({"images_epoch": image_table})
-    
 
-save_model_path = '/home/liruijun/projects/Generative_models/sd/trained_models'
+        pred_noise_imgs = pred_noise[:8]
+        img_grid = make_grid(pred_noise_imgs, nrow=8)
+    save_image(img_grid, f'/home/liruijun/projects/Generative_models/sd/trained_models/img_{epoch}.png')
+
+    
+save_model_path = '/home/liruijun/projects/Generative_models/sd/trained_models/flower.pth'
 print(f'finished training, saving model to - {save_model_path}')
 torch.save(model.state_dict(), save_model_path)
